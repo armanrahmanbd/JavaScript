0b9aa29f2129e2b920fc7c6170e46c9b3d35b234
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 00:32:55 +0600

    7

--- Basics/2.Operators/operators.js
+++ Basics/2.Operators/operators.js
@@ -9,4 +9,4 @@ let con1 = a > b; // false
 let con2 = a === 5; // true
 console.log('con1 && con2 =', con1 && con2); // false
 console.log('con1 || con2 =', con1 || con2); // true
-
+console.log('!con2 =', !con2); // false
