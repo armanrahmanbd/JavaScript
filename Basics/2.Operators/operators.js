d911f717da52e1e9a1b771cd2287fff67a418255
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 00:24:02 +0600

    3

---
+++ Basics/2.Operators/operators.js
@@ -0,0 +1,9 @@
+//Operators
+// 1. Arithmetic Operators
+// 2. Comparison Operators
+// 3. Logical Operators
+let a = 5;
+let b = 10;
+let con1 = a > b; // false
+let con2 = a === 5; // true
+console.log('con1 && con2:', con1 && con2); // false
