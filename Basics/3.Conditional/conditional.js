d78c164209ebdfa0b0d2be1e6e0ec9e1545c8acd
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 01:04:01 +0600

    9

---
+++ Basics/3.Conditional/conditional.js
@@ -0,0 +1,13 @@
+// if else Conditional Statement
+// Can Drive or Not?
+// If the person is 18 or above, they can drive. Otherwise, they cannot.
+console.log('Let\'s check if you can drive or not!');
+const userAge = prompt("Please enter your age:");
+console.log(`Your age is ${userAge} years.`);
+// console.log('Your age is ' + userAge + ' years.');
+// if else Conditional Statement
+if (userAge >= 18){
+  console.log("You can drive");
+} else{
+  console.log("You cannot drive");
+}
