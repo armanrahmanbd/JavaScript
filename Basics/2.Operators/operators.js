30545a535b75924ffaeb12f235840a893f78ce94
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 00:28:03 +0600

    4

--- Basics/2.Operators/operators.js
+++ Basics/2.Operators/operators.js
@@ -1,9 +1,12 @@
 //Operators
-// 1. Arithmetic Operators
-// 2. Comparison Operators
-// 3. Logical Operators
+// 1. Arithmetic Operators (+, -, *, /, %)
+// 2. Assignment Operators (=, +=, -=, *=, /=, %=)
+// 3. Comparison Operators (==, !=, >, <)
+// 4. Logical Operators (&&, ||, !)
 let a = 5;
 let b = 10;
 let con1 = a > b; // false
 let con2 = a === 5; // true
 console.log('con1 && con2:', con1 && con2); // false
+console.log('con1 || con2:', con1 || con2); // true
+
