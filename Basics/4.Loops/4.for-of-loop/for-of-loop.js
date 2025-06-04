f9a41c1bd0d5ead1da39acaa5355acc43f278698
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 15:24:34 +0600

    21

---
+++ Basics/4.Loops/4.for-of-loop/for-of-loop.js
@@ -0,0 +1,10 @@
+// JavaScript for of Loop
+let str = 'ArmanRahman';
+let strLength = 0;
+
+
+for (let i of str) {
+  console.log(`i = ${i}`);
+  strLength++;
+}
+console.log(`String Length: ${strLength}`);
