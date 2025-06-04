75baf555bf97fd02230e215ccbb8b01541815d49
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-04 14:58:13 +0600

    16

---
+++ Practices/Learnings/2.forLoopSum/forLoopSum.js
@@ -0,0 +1,7 @@
+// Practice for Loop Sum
+// Write a function that takes a list of integers as input and returns the sum of all the integers
+let sum = 0;
+for (let i=1; i<=100; i++) {
+  sum += i;
+}
+console.log('Sum', sum) // Output: 210
