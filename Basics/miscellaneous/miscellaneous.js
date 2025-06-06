e531e24115c61e4cc33c52af57a3ff4ad97de2d1
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 12:59:51 +0600

    31

--- Basics/miscellaneous/miscellaneous.js
+++ Basics/miscellaneous/miscellaneous.js
@@ -3,5 +3,4 @@ let str='My Name Is MD Arman RAHMAN';
 console.log(`Your String is "${str}"`);
 
 console.log(`The uppercase version of your given string is "${str.toUpperCase()}" by using toUpperCase() method`);
-console.log(`The lowercase version of your given string is "${str.toLowerCase()}" by using toLowerCase() method`);  
-console.log(`The removed whitespaces version of your string is "${str.trim()}" by using trim() method`);
+console.log(`The lowercase version of your given string is "${str.toLowerCase()}" by using toLowerCase() method`);
