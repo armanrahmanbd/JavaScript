a21015d6a0da6f8952939645526a37642f94fa76
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 12:59:16 +0600

    30

---
+++ Basics/miscellaneous/miscellaneous.js
@@ -0,0 +1,7 @@
+// JavaScript Miscellaneous
+let str='My Name Is MD Arman RAHMAN';
+console.log(`Your String is "${str}"`);
+
+console.log(`The uppercase version of your given string is "${str.toUpperCase()}" by using toUpperCase() method`);
+console.log(`The lowercase version of your given string is "${str.toLowerCase()}" by using toLowerCase() method`);  
+console.log(`The removed whitespaces version of your string is "${str.trim()}" by using trim() method`);
