f1a47caac9c84f4ef45b1d9c270406c1ddc37e4c
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 23:14:44 +0600

    62

---
+++ Basics/6.Function/function.js
@@ -0,0 +1,25 @@
+// JavaScript Function
+
+// Function to print hello world
+function printHelloWorld() {
+  console.log("Hello World!");
+}
+printHelloWorld();
+
+// a function that takes two parameters x and y, and returns their sum
+function add(x, y) {
+  return x + y;
+}
+console.log(add(989, 10));
+
+// Arrow function to print hello world
+const printHelloWorldArrow = () => {
+  console.log("Hello World!");
+}
+printHelloWorldArrow();
+
+// Function to print the multification of two numbers using arrow function
+const mulNumber = (x, y) => {
+  return x * y;
+}
+console.log(mulNumber(5, 12));
