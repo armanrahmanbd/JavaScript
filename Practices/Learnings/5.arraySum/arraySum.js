5a88a89ec07c4978e199e83f9e53c6f0615397c4
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 16:36:28 +0600

    46

--- Practices/Learnings/5.arraySum/arraySum.js
+++ Practices/Learnings/5.arraySum/arraySum.js
@@ -1,2 +1,8 @@
 // For a given array with marks of students -> [85, 97, 44, 37, 76, 60] Find the average marks of the entire class.
 
+let stuMarks = [85, 97, 44, 37, 76, 60];
+let sum = 0;
+for (let mark of stuMarks){
+  sum += mark;
+};
+console.log(`The average of student marks is ${sum/stuMarks.length}`);
