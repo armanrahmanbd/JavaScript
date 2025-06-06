3d88b8b611f57c46f4584542ede8665e63301661
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 13:08:36 +0600

    32

--- Basics/miscellaneous/miscellaneous.js
+++ Basics/miscellaneous/miscellaneous.js
@@ -4,3 +4,4 @@ console.log(`Your String is "${str}"`);
 
 console.log(`The uppercase version of your given string is "${str.toUpperCase()}" by using toUpperCase() method`);
 console.log(`The lowercase version of your given string is "${str.toLowerCase()}" by using toLowerCase() method`);
+console.log(`The sliced version of your given string is "${str.slice(11, 26)}" by using slice() method`); // ${str.slice(11)} would also work
