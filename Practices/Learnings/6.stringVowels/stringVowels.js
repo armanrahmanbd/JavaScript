7a6821dafaa53a0993b2c0c24b527630ef3e124b
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 00:19:56 +0600

    63

---
+++ Practices/Learnings/6.stringVowels/stringVowels.js
@@ -0,0 +1,12 @@
+// Find String Vowels using Functions
+
+function findVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let vow of vowels) {
+    count += str.split(vow).length - 1;
+  }
+  return count;
+}
+// Test the function
+console.log(findVowels("Hello World")); // Output: 3
