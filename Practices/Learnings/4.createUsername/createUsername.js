d82a4527d261a7de72986a05654b11c43d843f77
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 14:06:30 +0600

    37

---
+++ Practices/Learnings/4.createUsername/createUsername.js
@@ -0,0 +1,2 @@
+// user will input their names and the code will generate a username with the name with adding @ before and the name char length at the last e.g., name= Arman Rahman, the user name should be @armanrahman11
+
