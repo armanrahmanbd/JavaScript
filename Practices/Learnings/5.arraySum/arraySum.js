bd98567ff21083b77f0cf1203895d2612a500757
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-06 18:24:29 +0600

    49

--- Practices/Learnings/5.arraySum/arraySum.js
+++ Practices/Learnings/5.arraySum/arraySum.js
@@ -10,9 +10,17 @@ console.log(`The average of student marks is ${sum/stuMarks.length}`);
 // For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
 
 // using for loop
-let itemPrices = [250, 645, 300, 900, 50];
+let itemPrices1 = [250, 645, 300, 900, 50];
+
 for (let i=0; i<itemPrices.length; i++){
-   let itemsOffer = itemPrices[i]/ 10;
-   itemPrices[i] -= itemsOffer;
+   let itemsOffer = itemPrices1[i]/ 10;
+   itemPrices1[i] -= itemsOffer;
 }
-console.log(itemPrices);
+console.log(itemPrices1);
+
+// using for of loop
+ let itemPrices2 = [250, 645, 300, 900, 50];
+let i = 0;
+for (let prices of itemPrices2){
+  let 
+}
