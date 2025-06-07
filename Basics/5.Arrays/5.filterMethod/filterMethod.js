30600312d6057a5491d8bce2abb193844634cce2
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 11:30:01 +0600

    75

--- Basics/5.Arrays/5.filterMethod/filterMethod.js
+++ Basics/5.Arrays/5.filterMethod/filterMethod.js
@@ -2,9 +2,13 @@
 const Nums = [11, 22, 33, 44, 55, 66, 77, 88, 99];
 console.log(`Original Array: ${Nums}`);
 
-// Using map() to create a new array with each element multiplied by 2
-let mulNums = Nums.map((num) => {
-  return num * 2;
+// Using filter() to create a new array where all numbers are even
+let evenNums = Nums.filter((num) => {
+  return num % 2 === 0;
 });
-// Displaying the original and new arrays
-console.log(`New Array: ${mulNums}`);
+console.log(`New Even Numbers Array: ${evenNums}`);
+// Using filter() to create a new array where all numbers are greater than 50
+let greaterThan50 = Nums.filter((num) => {
+  return num > 50;
+});
+console.log(`New Numbers Greater Than 50 Array: ${greaterThan50}`);
