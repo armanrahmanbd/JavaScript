0fdc63f8912566a33c3d8a3468e9055da7ae6458
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 11:41:18 +0600

    78

--- Basics/5.Arrays/6.reduceMethod/reduceMethod.js
+++ Basics/5.Arrays/6.reduceMethod/reduceMethod.js
@@ -1,2 +1,10 @@
 // JavaScript Array reduce() Method Example
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(`Original Array: ${nums}`);
+
+// Using reduce() to calculate the sum of all numbers in the array
+let sum = nums.reduce((res, curr) => {
+  return res + curr;
+});
+console.log(`Sum of all numbers: ${sum} using reduce() Method`);
