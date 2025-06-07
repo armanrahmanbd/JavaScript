8adf405b47863a7cc9b0ac9339b53665e87177b6
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 11:45:32 +0600

    79

--- Basics/5.Arrays/6.reduceMethod/reduceMethod.js
+++ Basics/5.Arrays/6.reduceMethod/reduceMethod.js
@@ -1,6 +1,6 @@
 // JavaScript Array reduce() Method Example
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nums = [17540, 2000754, 874533, 974, 589, 60, 7077, 8985, 900007, 108644];
 console.log(`Original Array: ${nums}`);
 
 // Using reduce() to calculate the sum of all numbers in the array
@@ -8,3 +8,9 @@ let sum = nums.reduce((res, curr) => {
   return res + curr;
 });
 console.log(`Sum of all numbers: ${sum} using reduce() Method`);
+
+// find out the largest number in the array by using reduce() method
+let largest = nums.reduce((res, curr) => {
+  return res > curr ? res : curr;
+});
+console.log(`Largest number in the array: ${largest} using reduce() Method`);
