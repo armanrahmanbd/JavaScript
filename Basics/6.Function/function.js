c2d1baefb111f7b50284ef0c3a3f550dc9d1d4c4
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 10:17:47 +0600

    69

--- Basics/6.Function/function.js
+++ Basics/6.Function/function.js
@@ -24,19 +24,3 @@ const mulNumber = (x, y) => {
 };
 console.log(mulNumber(5, 12));
 
-// forEach function to iterate over an array and print each element // only for arrays not strings
-const numbers = [1, 2, 3, 4, 5];
-numbers.forEach((number) => {
-  console.log(number);
-});
-
-let cityNames = ["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata"];
-// Function to print each city name using forEach
-cityNames.forEach((city, idx, arr) => {
-  console.log(city.toUpperCase(), idx, arr);
-});
-
-// Function to print the square of each number in an array using forEach
-numbers.forEach((number) => {
-  console.log(number * number);
-});
