1b41ab1ea1c2fa880f06334b1f5a5f887f40952d
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-06-07 17:35:33 +0600

    81

--- DOM/1.Access&Change/access&Change.js
+++ DOM/1.Access&Change/access&Change.js
@@ -2,3 +2,9 @@ let h1 = document.querySelector("h1");
 h1.innerText = `${h1.innerText} from JavaScript`;
 let p = document.querySelector("p");
 p.innerText = `${p.innerText}d using JavaScript`;
+
+let divs = document.querySelectorAll(".box");
+
+divs[0].innerHTML = "First Div";
+divs[1].innerText = "Second Div";
+divs[2].innerText = "Third Div";
